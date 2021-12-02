e8708bfcd6106b655dc579be98d365d6d327ce43
shawn <shawn@LAPTOP-G5KGVNPP>
2021-12-02 16:36:23 -0600

    Add project files.

---
+++ Form2.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RectangleRadioButton = new System.Windows.Forms.RadioButton();
+            this.SquareRadioButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(436, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(310, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RectangleRadioButton);
+            this.groupBox1.Controls.Add(this.SquareRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(15, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 116);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please Select";
+            // 
+            // RectangleRadioButton
+            // 
+            this.RectangleRadioButton.AutoSize = true;
+            this.RectangleRadioButton.Location = new System.Drawing.Point(34, 74);
+            this.RectangleRadioButton.Name = "RectangleRadioButton";
+            this.RectangleRadioButton.Size = new System.Drawing.Size(84, 19);
+            this.RectangleRadioButton.TabIndex = 1;
+            this.RectangleRadioButton.Text = "Rectangle";
+            this.RectangleRadioButton.UseVisualStyleBackColor = true;
+            this.RectangleRadioButton.CheckedChanged += new System.EventHandler(this.RectangleRadioButton_CheckedChanged);
+            // 
+            // SquareRadioButton
+            // 
+            this.SquareRadioButton.AutoSize = true;
+            this.SquareRadioButton.Checked = true;
+            this.SquareRadioButton.Location = new System.Drawing.Point(34, 31);
+            this.SquareRadioButton.Name = "SquareRadioButton";
+            this.SquareRadioButton.Size = new System.Drawing.Size(68, 19);
+            this.SquareRadioButton.TabIndex = 0;
+            this.SquareRadioButton.TabStop = true;
+            this.SquareRadioButton.Text = "Square";
+            this.SquareRadioButton.UseVisualStyleBackColor = true;
+            this.SquareRadioButton.CheckedChanged += new System.EventHandler(this.SquareRadioButton_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(77, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 20);
+            this.label1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(77, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 20);
+            this.label2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(92, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Press enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 43);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form2";
+            this.Text = "Outdoor Illumination Calculator";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton RectangleRadioButton;
+        private System.Windows.Forms.RadioButton SquareRadioButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string lstBoxLine1 = "80 Watt LED Flood Light, 10,560 Lumens";
+            string lstBoxLine2 = "150 Watt LED Flood Light, 21,000 Lumens";
+            string lstBoxLine3 = "200 Watt LED Area Light, 28,000 Lumens";
+            //Add string to listbox
+            listBox1.Items.Add(lstBoxLine1);
+            listBox1.Items.Add(lstBoxLine2);
+            listBox1.Items.Add(lstBoxLine3);            
+            textBox2.Visible = false;
+            label1.Text = "Enter the side of the square";
+            //label2.Text = "Enter the height of the rectangle";
+        }
+        private void SquareRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            //making textBox2 visibility false
+            textBox2.Visible = false;
+            //setting label1.Text
+            label1.Text = "Enter the side of the square";
+            //making label2 visibility false
+            label2.Visible = false;
+            // making empty textBox1.Text, textBox2.Text and ReusultLabel.Text
+            textBox1.Text = "";
+            textBox2.Text = "";
+            //Clear listbox selection
+            listBox1.ClearSelected();
+        }
+
+        private void RectangleRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            //making textBox1 visibility true
+            textBox1.Visible = true;
+            //making textBox2 visibility true
+            textBox2.Visible = true;
+            //setting label1.Text
+            label1.Text = "Enter the length of the rectangle";
+            //setting label2.Text
+            label2.Text = "Enter the height of the rectangle";
+            //making label1 visibility true
+            label1.Visible = true;
+            //making label2 visibility true
+            label2.Visible = true;
+            //Clear listbox selection
+            listBox1.ClearSelected();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // making empty textBox1.Text, textBox2.Text
+            textBox1.Text = "";
+            textBox2.Text = "";
+            //making checked property false for RectangleRadioButton and SquareRadioButton
+            RectangleRadioButton.Checked = false;
+            SquareRadioButton.Checked = false;
+            listBox1.ClearSelected();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //declaring variable length as integer
+            int length;
+            //declaring variable width as integer
+            int width;
+            //declaring variable area as integer
+            float area = 0;
+            //declaring variable side as integer
+            int side;
+            //checking SquareRadioButton checked or not
+            if (SquareRadioButton.Checked)
+            {
+                //Convert to int and check if value entered by the user is a number
+                bool validSideValue = int.TryParse(textBox1.Text, out side);
+                if (!validSideValue)
+                {
+                    MessageBox.Show("Invalid entry for side of square.Only numbers are allowed", "Entry Error");
+                    // making empty textBox1.Text, textBox2.Text and ReusultLabel.Text
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+                else if (listBox1.SelectedIndex <= -1)
+                {
+                    MessageBox.Show("Select the lumens to calculate the number of fixtures", "Entry Error");
+                }
+                else
+                {
+                    //if checked, assigning textBox1.Text value to variable size
+                    //side = Convert.ToInt32(textBox1.Text);
+                    //calculating area by multiplying side * side
+                    area = side * side;
+                    //showing result in the message box
+                    MessageBox.Show("Area of Square is: " + Convert.ToString(area));
+                    calculateLumens(area);
+                }
+
+            }
+            else if (RectangleRadioButton.Checked)
+            {
+                bool validLengthValue = int.TryParse(textBox1.Text, out length);
+                bool validHeightValue = int.TryParse(textBox2.Text, out width);
+                if (!validLengthValue)
+                {
+                    MessageBox.Show("Invalid entry for length of rectangle.Only numbers are allowed", "Entry Error");
+                    // making empty textBox1.Text, textBox2.Text and ReusultLabel.Text
+                    textBox1.Text = "";
+                }
+                else if (!validHeightValue)
+                {
+                    MessageBox.Show("Invalid entry for width of rectangle.Only numbers are allowed", "Entry Error");
+                    // making empty textBox1.Text, textBox2.Text and ReusultLabel.Text
+                    textBox2.Text = "";
+                }
+                else if (listBox1.SelectedIndex <= -1)
+                {
+                    MessageBox.Show("Select the lumens to calculate the number of fixtures", "Entry Error");
+                    
+                }
+                else
+                {
+                    //if RectangleRadioButton checked
+                    //assigning textBox1.Text to variable length
+                    //length = Convert.ToInt32(textBox1.Text);
+                    //assigning textBox1.Text to variable width
+                    //width = Convert.ToInt32(textBox2.Text);
+                    //calculating area by multiplying length with width
+                    area = length * width;
+                    //showing result in the ReusultLabel label
+                    MessageBox.Show("Area of Rectangle is: " + Convert.ToString(area));
+                    calculateLumens(area);
+
+                }
+
+            }
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Function to calculate the fixture
+        private void calculateLumens(float area)
+        {
+            string selectedLumensText = listBox1.GetItemText(listBox1.SelectedItem);
+            string[] splitLumensText = selectedLumensText.Split(' ');
+            string lumenValue = splitLumensText[5];
+            float lumens = 0;
+            float candleLumens = 0;
+            float recomendlumens = 0;
+            float numFix = 0;
+            int numFixInt = 0;
+            lumenValue = lumenValue.Replace(",", "");
+            float.TryParse(textBox1.Text, out lumens);
+            if (area != 0)
+            {
+                candleLumens = (lumens / area);
+            }
+            else
+            {
+                candleLumens = 0;
+            }
+          ///recomended candle lumens is 1.5 for outdoor lighting
+            recomendlumens = (float)(1.50 * area);
+
+            numFix = candleLumens / recomendlumens;
+            //numFixInt = (int)Math.Ceiling(numFix);
+            string outString = String.Format("Recommended Lumens = {0}, Number of Fixtures = {1}", recomendlumens, numFix);
+            MessageBox.Show(outString, "Calculation Results");
+
+            
+
+        }
+
+        private void RectangleRadioButton_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
